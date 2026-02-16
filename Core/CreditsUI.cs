1eb3d6767c68d0f12ed369aa28b5248bf50c40c3
M1 Interactive <36132911+m1llipede@users.noreply.github.com>
2026-02-16 09:17:28 -0600

    hi

--- Core/CreditsUI.cs
+++ Core/CreditsUI.cs
@@ -6,16 +6,11 @@ namespace ASTRAS_WALL_WALK_ALWAYS.Core
     {
         GUIStyle? style;
 
-        void Start()
+        void OnGUI()
         {
             style = GUI.skin.label;
             style.fontSize = 14;
             style.normal.textColor = Color.white;
-        }
-
-        void OnGUI() 
-        {
-            
             GUI.Label(new Rect(10, 10, 400, 20), "CREDS LIST", style);
             GUI.Label(new Rect(10, 30, 400, 20), "Made by ASTRA", style);
             GUI.Label(new Rect(10, 50, 400, 20), "Thanks to iis menu temp for notfilib", style);
